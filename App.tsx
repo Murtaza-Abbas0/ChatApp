eec4b10bcd39bbf790b6300c7af610e90c246d25
murtazarizvi0 <133651433+murtazarizvi0@users.noreply.github.com>
2023-06-13 17:13:23 +0545

    Integrated React Navigation.

--- App.tsx
+++ App.tsx
@@ -3,12 +3,13 @@ import { View, Text } from 'react-native'
 import { store } from "./src/redux/store";
 import { Provider } from "react-redux";
 import LoginScreen from "./src/screens/LoginScreen";
+import NavigatinStack from "./src/components/navigation/NavigationStack";
 
 const App = () => {
   return (
     <Provider store={store} >
       <View style={{ flex: 1 }} >
-        <LoginScreen />
+        <NavigatinStack />
       </View>
     </Provider>
   )
---
+++ src/components/navigation/NavigationStack.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { LoginScreen } from '../../screens';
+
+const Stack = createNativeStackNavigator();
+const options = { headerShown: false }
+
+function NavigatinStack() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="LoginScreen" component={LoginScreen} options={options} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default NavigatinStack;
